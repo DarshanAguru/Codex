cbe6ebb9869f0f6fcd309451a319e87b95b43958
Darshan <agurudf@gmail.com>
2026-05-06 19:51:06 +0530

    Modified addFile.py, Modified assets/files.json, Added dsa/Rotate_image.txt, Added dsa/Rotate_list.txt, Added dsa/Rotate_string.txt, Added dsa/Rotated_digits.txt, Added dsa/Rotating_the_box.txt and 5 other files

--- addFile.py
+++ addFile.py
@@ -1,10 +1,15 @@
 import sys
+import os
 
 def addFile(name):
     name = name.lower()
     name = name.replace(" ", "_")
     name = name.replace("-", "_")
     name = name[0].upper() + name[1:]
+    files = os.listdir("dsa")
+    if f"{name}.txt" in files:
+        print(f"File {name}.txt already exists")
+        return    
     with open(f"dsa/{name}.txt", "w") as f:
         f.write("")
     print(f"Created file: dsa/{name}.txt")
